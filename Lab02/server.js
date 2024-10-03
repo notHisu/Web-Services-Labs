f158d3befa620bc1f0bd69387eb41e64e55e1e62
Le Trung Hoa Hieu <nothisu.dev@gmail.com>
2024-10-03 18:54:07 +0700

    feat: add Trigger Error

--- Lab02/server.js
+++ Lab02/server.js
@@ -56,6 +56,50 @@ app.post("/login", (req, res) => {
   }
 });
 
+// Route to trigger 1xx status code (Informational)
+app.get("/informational", (req, res) => {
+  res.status(100).send("Informational response");
+});
+
+// Route to trigger 2xx status code (Success)
+app.get("/success", (req, res) => {
+  res.status(200).send("Success");
+});
+
+// Route to trigger 3xx status code (Redirection)
+app.get("/redirection", (req, res) => {
+  res.status(301).redirect("/"); // Redirect to the home page
+});
+
+// Route to trigger 4xx status code (Client error)
+app.get("/client-error", (req, res) => {
+  res.status(400).send("Client error");
+});
+
+// Route to trigger 5xx status code (Server error)
+app.get("/server-error", (req, res) => {
+  res.status(500).send("Server error");
+});
+
+// Middleware to handle different status codes
+app.use((req, res, next) => {
+  const status = res.statusCode;
+
+  if (status >= 100 && status < 200) {
+    res.status(status).send("Informational response");
+  } else if (status >= 200 && status < 300) {
+    res.status(status).send("Success");
+  } else if (status >= 300 && status < 400) {
+    res.status(status).send("Redirection");
+  } else if (status >= 400 && status < 500) {
+    res.status(status).send("Client error");
+  } else if (status >= 500 && status < 600) {
+    res.status(status).send("Server error");
+  } else {
+    next();
+  }
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
